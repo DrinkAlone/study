4bfa1afd1113dff143f35e3a17647269dcccf7ae
追风少年 <44014117+DrinkAlone@users.noreply.github.com>
2019-04-10 15:22:55 +0800

    Add files via upload

---
+++ precache-manifest.8166cb2d41aad27f128332cc31ddb9d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "28fb9d958c6aff3bb208",
+    "url": "/static/js/main.b27d7b68.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "28fb9d958c6aff3bb208",
+    "url": "/static/css/main.4809b056.chunk.css"
+  },
+  {
+    "revision": "cd805a3cfbf91411b2ce90230547449a",
+    "url": "/index.html"
+  }
+];
